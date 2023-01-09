13b860f750245eb5a3847e5b9a37d7198720166d
natsu <reetam617@gmail.com>
2023-01-10 00:38:26 +0530

    adding search compo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Search from './components/Search/Search'
 
 const App = () => {
   return (
-    <div>Hello world</div>
+    <div>
+      <Search/>
+    </div>
   )
 }
 
